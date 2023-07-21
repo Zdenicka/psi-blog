cbd3046f5753d3b9fee47d1222c3775428dc5e33
Birma <birma@seznam.cz>
2023-07-21 20:55:57 +0200

    composition api a zpracování watch a watchEffect

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,60 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="home">
+      <h2>Home</h2>
+      <input type="text" v-model="search">
+      <p>search term - {{ search }}</p>
+      <p v-for="dog in matchingNames" :key="dog">{{ dog }}</p>
+      <q-btn @click="handleClick" label="Stop Watching" />
+
+  </div>
+
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<script>
+import { computed, ref, watch, watchEffect } from 'vue'
+
+export default {
+  name: 'psí blog',
+
+  setup () {
+
+    const search = ref('')
+    const dogs = ref([ 'Charlie', 'Bohoušek', 'Adam', 'Dorotka'])
+
+    const stopWatch = watch(search, () => {
+      console.log('watch function run')
+    })
+
+    const stopEffect = watchEffect(() => {
+      console.log('watchEffect', search.value)
+    })
+
+
+    const matchingNames = computed(() => {
+      return dogs.value.filter((dog) => dog.includes(search.value))
+    
+    })
+
+    const handleClick = () =>{
+        stopWatch()
+        stopEffect()
+    }
+
+
+    return { dogs, search, matchingNames, handleClick }
+
+  },
+}
+</script>
+
+
+
+
+
+<style lang="scss">
+.home {
   text-align: center;
-  color: #2c3e50;
+  color: $primary
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
